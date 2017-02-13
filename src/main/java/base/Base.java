91082c70306d85bbcab1235a33698182048f6be4
ITU <lai_christophe@hotmail.com>
2017-02-13 09:25:13 +0300

    test commit

--- src/main/java/base/Base.java
+++ src/main/java/base/Base.java
@@ -11,7 +11,10 @@ package base;
  */
 public interface Base 
 {
-    String username="postgres";
-    String password="itu";
-    String connURL="jdbc:postgresql://localhost:5432/ticom";
+//    String username="postgres";
+//    String password="itu";
+//    String connURL="jdbc:postgresql://localhost:5432/ticom";
+    String username="hapjjucaiquqzm";
+    String password="df9db5dc1eaca1a5a20cadf76137587905e1e093ce74a7f2703026ce9f4f8c45";
+    String connURL="jdbc:postgresql://ec2-50-19-125-201.compute-1.amazonaws.com:5432/d9eghjo91rqif1";
 }
